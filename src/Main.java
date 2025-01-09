fe3743673b18faeb94b3a408b98e725430ceec38
Fredrik Welander <welandfr@arcada.fi>
2025-01-09 11:58:33 +0200

    lektion 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello java!");
-    }
 
+        Car car = new Car("Toyota");
+        Car otherCar = new Car("Volvo");
+
+        System.out.println("Hello " + car.getName());
+        car.setName("Kia");
+        System.out.println("New name: " + car.getName());
+
+        System.out.println("Hello " + otherCar.getName());
+
+    }
 }
+
+
+
